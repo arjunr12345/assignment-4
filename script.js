147ec3f08faf8266193e46769b82229a1d5aaa3b
unknown <342572864@TDSB.ON.CA>
2025-02-27 15:14:21 -0500

    added code

--- script.js
+++ script.js
@@ -15,4 +15,25 @@ document.getElementById("quad-form").addEventListener('submit', (event) => {
         const rootOne = (-b + Math.sqrt(discriminant)) / (2 * a);
         document.getElementById("result").value = `x=${rootOne}`;
     }
-})
+}
+)
+document.getElementById("ambiguous").addEventListener('submit', (event) => {
+    event.preventDefault();
+    const a = document.getElementById("Aa").value;
+    const b = document.getElementById("sa").value;
+    const c = document.getElementById("sb").value;
+    const am = c*(Math.sin(a))
+    if (a<90) {
+    if (b<am) {
+        document.getElementById("result").value = "No triangle";
+    } else if (b=am) {
+        document.getElementById("result").value = "Right triangle";
+    } else if (b>am) {
+        document.getElementById("result").value = "one triangle";
+    } else if (am<b<c) {
+        document.getElementById("result").value = "Two triangles (ambiguous case)";
+    }
+}
+}
+
+)
